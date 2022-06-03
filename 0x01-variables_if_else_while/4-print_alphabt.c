01571c6990203a503e7a49a0a882401af0889921
etorojama <etorojama@yahoo.com>
2022-06-03 07:27:51 -0700

    print alphabets without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * 
+ * Description: Print all the letters except q and e
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
