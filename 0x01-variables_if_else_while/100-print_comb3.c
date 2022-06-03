b8e84544ec9d2c6153d30551876c4d9fb46d51ad
etorojama <etorojama@yahoo.com>
2022-06-03 08:57:55 -0700

    cdifferent combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ *
+ * Description: prints all different combinations of two digits.
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i , j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
