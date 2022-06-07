5fe711268387ee5f2bca121b82013d11f52c0844
etorojama <etorojama@yahoo.com>
2022-06-07 09:29:54 -0700

    refactored

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,27 @@ void times_table(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar((a * b) + '0');
+			product = a * b;
+			if (b == 0)
+			{
+				_putchar(product + '0');
+			}
+			else if (product >= 10)
+			{
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}
