ab1bdc53c087cb61ff785873b54be269e8fa590c
etorojama <etorojama@yahoo.com>
2022-06-07 07:41:20 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * Return: 0 on success
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+		return;
+	else
+		return abs(c);
+}
