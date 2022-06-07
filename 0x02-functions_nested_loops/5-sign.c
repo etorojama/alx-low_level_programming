440539175141f60fff492886657c1a907841262c
etorojama <etorojama@yahoo.com>
2022-06-07 07:26:10 -0700

    print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * print_sign - prints sign
+ *
+ * @n: integer to check
+ * Return: 0 on success
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
