c266057ff63b7c977f0f60b9353adb79b350d360
etorojama <etorojama@yahoo.com>
2022-06-07 05:46:15 -0700

    print lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - prints lower characters
+ *
+ * Return: 0 on success
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
