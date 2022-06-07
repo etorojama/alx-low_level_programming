9ca643f8bb1894542f1161ed4ba9a11d403ab7cf
etorojama <etorojama@yahoo.com>
2022-06-07 07:58:38 -0700

    added brackets

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,15 @@
 /**
  * _abs - computes the absolute value of an integer.
  *
+ * @c: integer to check
+ *
  * Return: 0 on success
  */
 
 int _abs(int c)
 {
 	if (c >= 0)
-		return (1);
+		return (c);
 	else
-		return c * -1;
+		return (c * -1);
 }
