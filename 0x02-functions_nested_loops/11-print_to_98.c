b98798e71562653a960c4b7f2c7bc011781512af
etorojama <etorojama@yahoo.com>
2022-06-08 01:33:56 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: integers to print
+ * Return: 0 on success
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		int c;
+
+		for (c = n; c > 98; c--)
+			_putchar(c + '0');
+	}
+	else
+	{
+		int c;
+
+		for (c = n; c < 98; c++)
+			_putchar(c + '0');
+	}
+}
