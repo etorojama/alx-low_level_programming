a0d46b4a9538a09f4d07d7dc6934baadc003c3bb
etorojama <etorojama@yahoo.com>
2022-06-08 03:14:03 -0700

    refactored code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,40 +9,17 @@
 
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n > 98)
 	{
-		_putchar(n);
+		for (; n > 98; n--)
+			printf("%d, ", n);
 	}
 	else if (n < 98)
 	{
-		int j;
-
-		for (j = n; j < 99; j++)
+		for (; n < 98; n++)
 		{
-			if (j == 98)
-			{
-				_putchar(j);
-			}
-			else
-			{
-				_putchar(j);
-			}
+			printf("%d, ", n);
 		}
 	}
-	else if (n > 98)
-	{
-		int j;
-
-		for (j = n; j > 97; j--)
-		{
-			if (j == 98)
-			{
-				_putchar(j);
-			}
-			else
-			{
-				_putchar(j);
-			}
-		}
-	}
+	printf("%d\n", n);
 }
