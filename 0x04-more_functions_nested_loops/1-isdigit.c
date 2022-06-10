7cf12dbf7ca3cd7125f8613152e4eeffbebc8256
etorojama <etorojama@yahoo.com>
2022-06-10 02:18:19 -0700

    added more comment

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,13 +5,12 @@
  * _isdigit - checks digit
  * @c: int to check
  *
- * Return: 0 on success
+ * Return: 1 if digit and 0 if anything else
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
-	else
-		return (0);
+	return (0);
 }
