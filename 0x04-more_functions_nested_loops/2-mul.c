12e41f01011f2581ae9a34c8fca6371454867aaa
etorojama <etorojama@yahoo.com>
2022-06-10 02:26:57 -0700

    multiplication of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies integers
+ * @a: integer to multiply
+ * @b: another integer
+ *
+ * Return: 0 on success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b)
+}
