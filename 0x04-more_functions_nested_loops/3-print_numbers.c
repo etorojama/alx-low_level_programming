49bb6719a744be0eb531df861192eeca060ad15d
etorojama <etorojama@yahoo.com>
2022-06-10 02:32:47 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+}
