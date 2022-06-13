b6e3d3d090df214645d091fb2d4ffec0da783fc7
etorojama <etorojama@yahoo.com>
2022-06-13 08:09:29 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s += 2;
+		i += 2;
+	}
+
+	_putchar('\n');
+}
