1e5ed38c4e1e1eac332d106c30c86d01cdc4370c
etorojama <etorojama@yahoo.com>
2022-06-22 17:06:53 -0700

    refactored

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,10 +7,11 @@
 
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if(*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
