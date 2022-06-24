c61b5176b58b7575f29373af1fa9333a073ee04b
etorojama <etorojama@yahoo.com>
2022-06-24 07:16:18 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: count variable
+ * @argv: pointer to string array
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
