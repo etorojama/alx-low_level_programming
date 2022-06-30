ab1886faefd49f704afc5cd7c6a6e4a7fafcb7e6
etorojama <etorojama@yahoo.com>
2022-06-30 14:22:14 -0700

    malloc echeck

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: argument to alloctae to
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int a;
+
+	a = malloc(sizeof(int) * b);
+
+	if (a == NULL)
+	{
+		exit(0);
+	}
+	else
+		return ((int *)(a));
