6fcd73ec4f44099b1bbb8452646f1fa15ae00d2c
etorojama <etorojama@yahoo.com>
2022-06-30 14:45:33 -0700

    added brace

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,23 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * malloc_checked - allocates memory using malloc
  * @b: amount of bytes
  *
- * Return: 0 on success
+ * Return: pointer to allocated memory.
  */
 
 void *malloc_checked(unsigned int b)
 {
-	int *a;
+	void *a;
 
 	a = malloc(b);
 
 	if (a == NULL)
-	{
 		exit(98);
-	}
 	
 	return (a);
+}
