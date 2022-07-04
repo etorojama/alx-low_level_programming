9c457415f25668e6795a7be61b13037d580b216b
etorojama <etorojama@yahoo.com>
2022-07-04 08:12:02 -0700

    added sreturn

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -7,6 +8,8 @@
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
+ *
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
