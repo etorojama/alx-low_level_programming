2168ee2d40cc60b27abc084d724010a2ef0419fe
etorojama <etorojama@yahoo.com>
2022-07-04 08:13:35 -0700

    added def

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: a struct called dog with three members
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
