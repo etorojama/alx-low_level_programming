500a7ddfdb2ff3da0478a8ce6f55db02c66f62e3
etorojama <etorojama@yahoo.com>
2022-07-07 08:02:07 -0700

    removed else

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,14 +16,12 @@ int sum_them_all(const unsigned int n, ...)
 	
 	if (n == 0)
 		return (0);
-	else
-	{
-		va_start(ap, n);
 
-		for (i = 0; i < n; i++)
-			sum += va_arg(ap, int);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
-		va_end(ap);
-		return (sum);
-	}
+	va_end(ap);
+
+	return (sum);
 }
