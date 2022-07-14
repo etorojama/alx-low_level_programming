613b0591b3b7a1e960fec4553662a8651e5cd322
etorojama <etorojama@yahoo.com>
2022-07-14 07:35:25 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t.
+ * @h: pointer to list.
+ *
+ * Return: 0 on success
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			count++;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
