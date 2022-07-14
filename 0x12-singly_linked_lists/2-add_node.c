63db51c2860edc166e506d70c10f987befca2e50
etorojama <etorojama@yahoo.com>
2022-07-14 07:41:55 -0700

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * _strlen - finds length of a string.
+ * @str: string.
+ *
+ * Return: 0 on success
+ */
+
+int _strlen(const char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t.
+ * @head: pointer to first element of the list.
+ * @str: string to be duplicated.
+ *
+ * Return: 0 on success
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->next = *head;
+	temp->str = strdup(str);
+	temp->len = _strlen(str);
+
+	*head = temp;
+
+	return (*head);
+}
