06ca2a5177d7ba6bfdb0917f19d402628b942757
etorojama <etorojama@yahoo.com>
2022-07-14 08:01:36 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * constructorRun - runs
+ *
+ */
+
+void constructorRun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
