ff15b16e2073793f245a058bf4a9abeb8c571acd
etorojama <etorojama@yahoo.com>
2022-07-19 07:13:20 -0700

    get node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a linked list
+ * @head: head of list.
+ * @index: index of the node
+ *
+ * Return: 0 on success
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+
+		i++, head = head->next;
+	}
+
+	return (NULL);
+}
