f81f94fd1528be55c39d122436f9a35e8096d01b
etorojama <etorojama@yahoo.com>
2022-07-19 07:10:13 -0700

    corrected return

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
  * pop_listint - Delete the head node of a list.
  * @head: A pointer
  *
- * return: 0 on success
+ * Return: 0 on success
  */
 
 int pop_listint(listint_t **head)
@@ -22,5 +22,6 @@ int pop_listint(listint_t **head)
 	free(*head);
 
 	*head = temp;
+
 	return (data);
 }
