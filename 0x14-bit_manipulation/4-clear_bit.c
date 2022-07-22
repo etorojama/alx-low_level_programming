280032f7cae4df26144aa6ba0ab17703be062152
etorojama <etorojama@yahoo.com>
2022-07-22 07:51:17 -0700

    removed helper

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * getp - returns the value of x to the power of y.
- * @x: number.
- * @y: power.
- *
- * Return: 0 on success
- */
-
-int getp(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	if (y == 1)
-		return (x);
-	return (x * getp(x, y - 1));
-}
-
-/**
  * clear_bit - stes the value of a bit to one at a given index.
  * @n: pointer to number
  * @index: index of bit.
@@ -31,12 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-	if (n)
-	{
-		*n -= getp(2, index);
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+
+	*n = *n & ~(1 << index);
+	return (1);
+}
