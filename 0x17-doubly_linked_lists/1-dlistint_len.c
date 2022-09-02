3b32d488e6fc24fded9d0c2f9d1c5a7a03b08cdf
etorojama <etorojama@yahoo.com>
2022-09-02 15:14:35 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in a lineked dlistint_t list
+ * @h: pointer to list.
+ *
+ * Return: 0 on success
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t count;
+
+	temp = h;
+	count = 0;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
